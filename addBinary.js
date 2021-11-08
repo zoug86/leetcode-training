1d936153178327dc3a2620bfd08460b54419dd3f
Houssem Marzougui <marzouguih86@gmail.com>
2021-11-08 11:52:39 -0800

    problem 14 new solution

--- addBinary.js
+++ addBinary.js
@@ -9,59 +9,77 @@
  * @return {string}
  */
 const addBinary = function (a, b) {
-    let arr1 = a.split('');
-    let arr2 = b.split('');
-    let l = Math.abs(arr1.length - arr2.length);
-    if (arr1.length > arr2.length) {
-        for (let i = 0; i < l; i++) {
-            arr2.unshift('0')
-        }
-    }
-    if (arr1.length < arr2.length) {
-        for (let i = 0; i < l; i++) {
-            arr1.unshift('0')
-        }
-    }
-    console.log(arr1, arr2)
-    let arr = [];
-    let c = '0';
-    for (let i = arr1.length - 1; i >= 0; i--) {
-        if (arr1[i] === arr2[i]) {
-            if (arr1[i] === '1' && c === '1') {
-                arr.unshift('1');
-                c = '1';
-            } else if (arr1[i] === '0' && c === '1') {
-                arr.unshift('1');
-                c = '0'
-            } else if (arr1[i] === '0' && c === '0') {
-                arr.unshift('0');
-                c = '0'
-            } else {
-                arr.unshift('0');
-                c = '1'
-            }
-        } else {
-            if (arr1[i] === '1' && c === '1') {
-                arr.unshift('0');
-                c = '1';
-            } else if (arr1[i] === '0' && c === '1') {
-                arr.unshift('0');
-                c = '1'
-            } else if (arr1[i] === '0' && c === '0') {
-                arr.unshift('1');
-                c = '0'
-            } else {
-                arr.unshift('1');
-                c = '0'
-            }
-        }
-    }
-    if (c !== '0') {
-        arr.unshift(c);
-    }
+    // VERY LONG SOLUTION
+    // let arr1 = a.split('');
+    // let arr2 = b.split('');
+    // let l = Math.abs(arr1.length - arr2.length);
+    // if (arr1.length > arr2.length) {
+    //     for (let i = 0; i < l; i++) {
+    //         arr2.unshift('0')
+    //     }
+    // }
+    // if (arr1.length < arr2.length) {
+    //     for (let i = 0; i < l; i++) {
+    //         arr1.unshift('0')
+    //     }
+    // }
+    // let arr = [];
+    // let c = '0';
+    // for (let i = arr1.length - 1; i >= 0; i--) {
+    //     if (arr1[i] === arr2[i]) {
+    //         if (arr1[i] === '1' && c === '1') {
+    //             arr.unshift('1');
+    //             c = '1';
+    //         } else if (arr1[i] === '0' && c === '1') {
+    //             arr.unshift('1');
+    //             c = '0'
+    //         } else if (arr1[i] === '0' && c === '0') {
+    //             arr.unshift('0');
+    //             c = '0'
+    //         } else {
+    //             arr.unshift('0');
+    //             c = '1'
+    //         }
+    //     } else {
+    //         if (arr1[i] === '1' && c === '1') {
+    //             arr.unshift('0');
+    //             c = '1';
+    //         } else if (arr1[i] === '0' && c === '1') {
+    //             arr.unshift('0');
+    //             c = '1'
+    //         } else if (arr1[i] === '0' && c === '0') {
+    //             arr.unshift('1');
+    //             c = '0'
+    //         } else {
+    //             arr.unshift('1');
+    //             c = '0'
+    //         }
+    //     }
+    // }
+    // if (c !== '0') {
+    //     arr.unshift(c);
+    // }
+    // return arr.join("");
 
-    console.log(arr.join(""))
-    return arr.join("");
+    // SHORT SOLUTION
+    let carry = 0;
+    let result = '';
+
+    let n = a.length - 1;
+    let m = b.length - 1;
+
+    for (; n >= 0 || m >= 0 || carry > 0; n--, m--) {
+        let sum = (+a[n] || 0) + (+b[m] || 0) + carry;
+        if (sum > 1) {
+            sum = sum % 2;
+            carry = 1
+        } else {
+            carry = 0;
+        }
+        result = `${sum}${result}`
+    }
+    console.log(result)
+    return result;
 };
 
 addBinary(a = "11101", b = "111")
