3fb4639106da7978471409740be76145f0ad67a1
Houssem Marzougui <marzouguih86@gmail.com>
2021-11-08 16:07:21 -0800

    problem 15 new solution

--- mySqrt.js
+++ mySqrt.js
@@ -10,7 +10,28 @@
  * @return {number}
  */
 const mySqrt = function (x) {
-    return Math.floor(Math.sqrt(x))
+    // immediate solution
+    //return Math.floor(Math.sqrt(x))
+
+    // finding the square root without using sqrt()
+    let left = 1;
+    let right = Math.floor(x / 2) + 1;
+    let mid;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (mid * mid > x) {
+            right = mid - 1;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+
+    return right;
+
 };
 
-console.log(mySqrt(8))
+console.log(mySqrt(24))
