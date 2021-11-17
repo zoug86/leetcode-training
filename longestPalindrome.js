8a894a49da4f6f6b6543d19dc4eb5d1af9032842
Houssem Marzougui <marzouguih86@gmail.com>
2021-11-16 21:29:51 -0800

    problem 24

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -8,28 +8,58 @@
  * @param {string} s
  * @return {string}
  */
+const longestPalindrome = function (s) {
+    // if (s.length <= 1) return s;
+    // let longPalin = '';
+    // let sub = '';
+    // for (let i = 0; i < s.length; i++) {
+    //     for (let j = i; j < s.length; j++) {
+    //         sub += s[j];
+    //         if ((sub.length > longPalin.length) &&
+    //             (sub.substring(0, Math.floor(sub.length / 2)) ===
+    //                 sub.substring(sub.length % 2 === 0 ? Math.floor(sub.length / 2) : Math.floor(sub.length / 2) + 1, sub.length).split('').reverse().join(''))) {
+    //             longPalin = sub;
+    //         }
+    //     }
+    //     sub = '';
+    // }
+    // return longPalin;
+    let max = '';
+    for (let i = 0; i < s.length; i++) {
+        // this loop is to take into account 
+        // different palindromes like 'aba' and 'abba'
+        for (let j = 0; j < 2; j++) {
+            let left = i;
+            let right = i + j;
+            while (left >= 0 && s[left] === s[right]) {
+                left--;
+                right++;
+            }
+            // here imagine we get into string like
+            // "sabbad", then
+            // right = 5
+            // left = 0
+            // and actual length of "abba" should be "4"
+            // 5 - 0 - 1 === 4
+            if ((right - left - 1) > max.length) {
+                max = s.substring(left + 1, right);
+            }
 
-// depp clones objects, arrays and functions
-const superClone = (object) => {
-    let cloning = {};
+        }
+        // No better move exists
+        if (Math.ceil(max.length / 2) >= s.length - i) break;
+    }
+    return max;
+};
 
-    Object.keys(object).map(prop => {
-        if (Array.isArray(object[prop])) {
-            cloning[prop] = [].concat(object[prop])
-        } else if (typeof object[prop] === 'object') {
-            cloning[prop] = superClone(object[prop])
-        } else cloning[prop] = object[prop]
-    })
+console.log(longestPalindrome("babad"))
+console.log(longestPalindrome("cbbd"))
+console.log(longestPalindrome("ac"))
+console.log(longestPalindrome("a"))
+console.log(longestPalindrome("cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj"))
 
-    return cloning
-}
-
-let obj = { a: 1, b: 2 }
-
-let obj2 = superClone(obj);
-
-console.log(obj2 === obj)
-
-obj2.a = 10;
-
-console.log(obj2, obj)
+// let sub = 'bab';
+// let temp = sub.substring(0, Math.floor(sub.length / 2))
+// console.log(temp)
+// console.log(temp)
+// console.log(sub.indexOf(temp.split('').reverse().join('')) !== -1)
